ebb433592e7f548299c49f5674d8013c9c6e7dbc
vlevitskiy86 <35646434+vlevitskiy86@users.noreply.github.com>
2018-09-25 18:50:48 +0300

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatApi
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            string name = Context.User.Identity.Name;
+            Groups.AddToGroupAsync(Context.ConnectionId, name);
+
+            return base.OnConnectedAsync();
+        }
+
+        //public async Task SendMessage(string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
+        //}
+    }
+}
